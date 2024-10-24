9b35c5ea1de3e7ca591fd30e7ca555ddc01eb83b
amithsoddy <anithasoddy@gmail.com>
2024-10-24 20:38:30 +0530

    robot

---
+++ robot.py
@@ -0,0 +1,12 @@
+class robot:
+    grade=10
+    name="alexa"
+    def intro(self):
+         print("hello i am a robot")
+    def details(self):
+        print(self.grade,self.name)
+ob=robot()
+ob.intro()
+ob.details()
+
+    
